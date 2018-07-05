bd2d3471df27fd81a8846d0661c9040a619c6c5e
446734796@qq.com <wuqiang502>
2018-07-05 21:41:59 +0800

    north brain first

---
+++ demo_module_0/src/main/java/com/northbrain/demo_module_0/DemoModule0Application.java
@@ -0,0 +1,12 @@
+package com.northbrain.demo_module_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModule0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoModule0Application.class, args);
+	}
+}
