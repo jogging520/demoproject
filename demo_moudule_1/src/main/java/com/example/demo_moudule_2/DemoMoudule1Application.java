ec2c4680372347c40bdcb031263c61a3b4c85646
wq <446734796@qq.com>
2018-07-05 21:50:50 +0800

    wq

---
+++ demo_moudule_1/src/main/java/com/example/demo_moudule_2/DemoMoudule1Application.java
@@ -0,0 +1,12 @@
+package com.example.demo_moudule_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMoudule1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMoudule1Application.class, args);
+    }
+}
