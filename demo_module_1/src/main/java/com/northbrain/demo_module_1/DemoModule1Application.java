f737301780760b49f13aaa8447f9f36fbbd8f589
446734796@qq.com <wuqiang502>
2018-07-05 21:50:16 +0800

    jiakun's first

---
+++ demo_module_1/src/main/java/com/northbrain/demo_module_1/DemoModule1Application.java
@@ -0,0 +1,12 @@
+package com.northbrain.demo_module_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoModule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoModule1Application.class, args);
+	}
+}
